276f5fd613c20cb9addbca1acef24934b223eef0
Reuben-24 <reubengfaltiska@gmail.com>
2025-08-24 00:16:07 +1000

    Add Muscle Group routes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 require("dotenv").config();
 
-const usersRouter = require("./routes/usersRouter.js")
-const exercisesRouter = require("./routes/exercisesRouter.js")
+const usersRouter = require("./routes/usersRouter")
+const exercisesRouter = require("./routes/exercisesRouter")
+const muscleGroupsRouter = require("./routes/muscleGroupsRouter")
 
 app = express();
 
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", usersRouter);
 app.use("/users/:userId/exercises", exercisesRouter)
+app.use("/users/:userId/muscle-groups", muscleGroupsRouter)
 
 // Centralized error handler
 app.use((err, req, res, next) => {
---
+++ controllers/muscleGroupsController.js
@@ -0,0 +1,70 @@
+const muscleGroup = require("../models/MuscleGroup");
+
+exports.create = async (req, res) => {
+  // TO DO ADD INPUT VALIDATION
+  const userId = req.params.userId;
+  const { name } = req.body;
+  const data = { user_id: userId, name };
+  const newMuscleGroup = await muscleGroup.create(data);
+  res.status(201).json({
+    message: "Muscle Group successfully created",
+    muscleGroup: newMuscleGroup,
+  });
+};
+
+exports.readAllForUser = async (req, res) => {
+  const userId = req.params.userId;
+  const muscleGroups = await muscleGroup.getAllByUserId(userId);
+
+  res.status(200).json({
+    message: "Muscle Groups successfully retrieved",
+    muscleGroups, // will be [] if no exercises
+  });
+};
+
+exports.readForUserById = async (req, res) => {
+  const { userId, muscleGroupId } = req.params;
+  const existingMuscleGroup = await muscleGroup.getById(muscleGroupId);
+
+  if (!existingMuscleGroup || existingMuscleGroup.user_id !== Number(userId)) {
+    return res.status(404).json({ error: "Muscle Group not found" });
+  }
+
+  res.status(200).json({
+    message: "Muscle Group successfully retrieved",
+    muscleGroup: existingMuscleGroup,
+  });
+};
+
+exports.update = async (req, res) => {
+  // TODO: ADD INPUT VALIDATION
+  const { userId, muscleGroupId } = req.params;
+  const fieldsToUpdate = { ...req.body };
+
+  const updatedMuscleGroup = await muscleGroup.update(muscleGroupId, fieldsToUpdate);
+
+  if (!updatedMuscleGroup || updatedMuscleGroup.user_id !== Number(userId)) {
+    return res.status(404).json({ error: "Muscle Group not found" });
+  }
+
+  res.status(200).json({
+    message: "Muscle Group successfully updated",
+    muscleGroup: updatedMuscleGroup,
+  });
+};
+
+exports.delete = async (req, res) => {
+  const { userId, muscleGroupId } = req.params;
+
+  const existingMuscleGroup = await muscleGroup.getById(muscleGroupId);
+  if (!existingMuscleGroup || existingMuscleGroup.user_id !== Number(userId)) {
+    return res.status(404).json({ error: "Muscle Group not found" });
+  }
+
+  const deletedMuscleGroup = await muscleGroup.delete(muscleGroupId);
+
+  res.status(200).json({
+    message: "Muscle Group successfully deleted",
+    muscleGroup: deletedMuscleGroup
+  })
+}
